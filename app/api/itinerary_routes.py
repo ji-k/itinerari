db5d5244d6ea03193097a0f2e85ca1886cf15d43
Ji <jikyung@gmail.com>
2021-08-26 11:56:25 -0400

    POST, DELETE for itinerary_routes

--- app/api/itinerary_routes.py
+++ app/api/itinerary_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask_login import current_user
-from app.models import Itinerary
+from app.models import db, Itinerary
 from app.forms.itinerary_form import ItineraryForm
 
 itinerary_routes = Blueprint('itineraries', __name__, url_prefix='')
@@ -17,3 +17,27 @@ def get_itineraries():
 def get_itinerary(id):
     itinerary = Itinerary.query.get(id)
     return itinerary.to_dict()
+
+# edit an itinerary
+@itinerary_routes.route('/api/itineraries/create/', methods=['POST'])
+def create_itinerary():
+    form = ItineraryForm()
+    data = form.data
+    itinerary = ItineraryForm(
+        title=data['title'],
+        start_date=data['start_date'],
+        end_date=data['end_date'],
+        image_url=data['image_url'],
+        notes=data['notes']
+    )
+    db.session.add(itinerary)
+    db.session.commit()
+    return itinerary.to_dict()
+
+# delete an itinerary
+@itinerary_routes.route('<int:id>',methods=['DELETE'])
+def delete_itinerary(id):
+    itinerary = Itinerary.query.get(id)
+    db.session.delete(itinerary)
+    db.session.commit()
+    return itinerary.to_dict
