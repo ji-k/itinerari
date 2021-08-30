d908dbc7e16881fc475413883b232b31ce3e405b
Ji <jikyung@gmail.com>
2021-08-29 23:17:25 -0400

    complete landing page features

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -20,7 +20,25 @@ const LandingPage = () => {
                 </div>
                 <div className="landing-page__features-container">
                     <p className="features">Features</p>
-                    <p className="features-details">itinerari is a travel management app that allows users to create a single-page itinerary to organize and plan their travel logistics. Itineraries are fully customizable for a single traveler, or for small or large groups. </p>
+                    <p className="features-details">itinerari is a travel management app that allows users to create a single-page itinerary to organize and plan their travel logistics. itinerari hopes travelers can benefit from having their travel information conveniently all in one place, or to help with communication within a group of travelers.</p>
+                    <div className="flex-center-me">
+                        <div className="feature-1-container feature-points">
+                            <img src="https://github.com/ji-k/itinerari/blob/main/assets/images/1.jpg?raw=true" className="feature-image" />
+                            <p className="feature-points-details">Itineraries are fully customizable for a single traveler, or for small or large groups. </p>
+                        </div>
+                        <div className="feature-2-container feature-points">
+                            <img src="https://github.com/ji-k/itinerari/blob/main/assets/images/2.jpg?raw=true" className="feature-image" />
+                            <p className="feature-points-details">Add flight information for you or a group of travelers. Adding a passenger list is easy so you'll know who is flying where.</p>
+                        </div>
+                        <div className="feature-3-container feature-points">
+                            <img src="https://github.com/ji-k/itinerari/blob/main/assets/images/11.jpg?raw=true" className="feature-image" />
+                            <p className="feature-points-details">Add car rental information so you'll know exactly where to go to grab your car after leaving a hectic airport.</p>
+                        </div>
+                        <div className="feature-4-container feature-points">
+                            <img src="https://github.com/ji-k/itinerari/blob/main/assets/images/9.jpg?raw=true" className="feature-image" />
+                            <p className="feature-points-details">Add hotel information so you'll know exact dates of stay. Add a rooming list so you'll know who is staying where.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
