b0a75d05b131f08cf49fa74ae5b939be8e4758f0
Ji <jikyung@gmail.com>
2021-08-30 00:54:39 -0400

    update delete itinerary

--- react-app/src/components/ItineraryPage/ItineraryPage.js
+++ react-app/src/components/ItineraryPage/ItineraryPage.js
@@ -4,8 +4,6 @@ import EditItineraryModal from '../EditItineraryModal';
 import './ItineraryPage.css'
 
 export default function Profile({ number }) {
-
-
     const itineraries = Object.values(useSelector(state => state.itineraries))
     const itinerary = itineraries.filter(itinerary => (
         itinerary?.id == number
@@ -14,17 +12,21 @@ export default function Profile({ number }) {
 
     return (
         <>
-            < EditItineraryModal itinerary={itinerary} />
-            <div className="itinerary-page__outer-container">
-                <p>Title: {itinerary[0]?.title}</p>
-                <p>Trip Start: {itinerary[0]?.start_date}</p>
-                <p>Trip End: {itinerary[0]?.end_date}</p>
-                <p>Banner: {itinerary[0]?.image_url}</p>
-                <p>Notes: {itinerary[0]?.notes}</p>
+            {itinerary[0] &&
+                <div>
+                    < EditItineraryModal itinerary={itinerary} />
+                    <div className="itinerary-page__outer-container">
+                        <p>Title: {itinerary[0]?.title}</p>
+                        <p>Trip Start: {itinerary[0]?.start_date}</p>
+                        <p>Trip End: {itinerary[0]?.end_date}</p>
+                        <p>Banner: {itinerary[0]?.image_url}</p>
+                        <p>Notes: {itinerary[0]?.notes}</p>
 
-                <p>Airline: {itinerary[0]?.flight_info[0]?.airline}</p>
+                        <p>Airline: {itinerary[0]?.flight_info[0]?.airline}</p>
 
-            </div>
+                    </div>
+                </div>
+            }
         </>
     )
 }
