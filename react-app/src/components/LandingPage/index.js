5d27cb1981f9ee5bdcbd1750811ffe74f39edda7
Ji <jikyung@gmail.com>
2021-08-29 22:40:53 -0400

    update landing page, add assets

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -13,9 +13,15 @@ const LandingPage = () => {
     return (
         <>
             <div className="landing-page__outer-container">
-                <p className="tag-line">Create informative, single-page itineraries</p>
-                <p className="travel-smarter">TRAVEL SMARTER</p>
-                <button onClick={getStarted} className="getStarted-button">Get Started</button>
+                <div className="landing-page__header-container">
+                    <p className="tag-line">Create informative, single-page itineraries</p>
+                    <p className="travel-smarter">TRAVEL SMARTER</p>
+                    <button onClick={getStarted} className="getStarted-button">Get Started</button>
+                </div>
+                <div className="landing-page__features-container">
+                    <p className="features">Features</p>
+                    <p className="features-details">itinerari is a travel management app that allows users to create a single-page itinerary to organize and plan their travel logistics. Itineraries are fully customizable for a single traveler, or for small or large groups. </p>
+                </div>
             </div>
         </>
     )
