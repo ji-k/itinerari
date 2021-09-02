e06f13101b6affcedeb8f752ee55ccf5a07fd0f4
Ji <jikyung@gmail.com>
2021-09-01 23:09:51 -0400

    update login_form.py

--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -15,7 +15,7 @@ def user_exists(form, field):
 def password_matches(form, field):
     # Checking if password matches
     password = field.data
-    email = form.data['Email']
+    email = form.data['email']
     user = User.query.filter(User.email == email).first()
     if not user:
         raise ValidationError('This account does not exist')
