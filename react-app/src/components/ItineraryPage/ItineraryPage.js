f916a571979a4476bf2b2e50f6c886c26666fbfc
Ji <jikyung@gmail.com>
2021-09-02 16:17:33 -0400

    update

--- react-app/src/components/ItineraryPage/ItineraryPage.js
+++ react-app/src/components/ItineraryPage/ItineraryPage.js
@@ -49,7 +49,7 @@ export default function ItineraryPage({ number }) {
                         {/* ************* Itinerary ************* */}
                         <div className="general-info__title">{itinerary?.title}</div>
                         <div className="general-info__outer-container">
-                            <div className="general-info__sDate">
+                            <div className="general-info__Date">
                                 Trip Dates: {(new Date(itinerary?.start_date).toDateString())} - {(new Date(itinerary?.end_date).toDateString())}
                             </div>
                             {/* <div className="general-info__sDate">Trip Start: {itinerary?.start_date}</div>
