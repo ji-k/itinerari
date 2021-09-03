a3d11c46a92e244e966251ed59495e1ed04f4771
Ji <jikyung@gmail.com>
2021-09-03 13:38:25 -0400

    remove print

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -84,7 +84,6 @@ def sign_up():
         db.session.commit()
         login_user(user)
         return user.to_dict()
-    print(form.errors)
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
     return form.errors, 401
 
