74967d090cec57965b13885f7b533033f98cf9ad
Ji <jikyung@gmail.com>
2021-09-02 22:00:42 -0400

    style CreateFlightFormModal

--- react-app/src/components/Flight/CreateFlightForm.js
+++ react-app/src/components/Flight/CreateFlightForm.js
@@ -38,41 +38,108 @@ const CreateFlightForm = ({ setShowModal, itinerary_id }) => {
 
     return (
         <>
-            <div className="flight-form__container">
-                <form className="flight-form" onSubmit={handleSubmit}>
-                    <label>
-                        Date
-                    </label>
-                    <input type="date" value={date} placeholder="Trip Date" onChange={(e) => setDate(e.target.value)} />
-                    <label>
-                        Origin
-                    </label>
-                    <input type="string" value={origin} placeholder="Origin" onChange={(e) => setOrigin(e.target.value)} />
-                    <label>
-                        Destination
-                    </label>
-                    <input type="string" value={destination} placeholder="Destination" onChange={(e) => setDestination(e.target.value)} />
-                    <label>
-                        Departure
-                    </label>
-                    <input type="string" value={departure} placeholder="Departure" onChange={(e) => setDeparture(e.target.value)} />
-                    <label>
-                        Arrival
-                    </label>
-                    <input type="string" value={arrival} placeholder="Arrival" onChange={(e) => setArrival(e.target.value)} />
-                    <label>
-                        Airline
-                    </label>
-                    <input type="string" value={airline} placeholder="Airline" onChange={(e) => setAirline(e.target.value)} />
-                    <label>
-                        Flight No.
-                    </label>
-                    <input type="string" value={flight_no} placeholder="Flight No." onChange={(e) => setFlight_no(e.target.value)} />
-                    <label>
-                        Notes
-                    </label>
-                    <input type="text" value={notes} placeholder="Notes" onChange={(e) => setNotes(e.target.value)} />
-                    <button type="submit">New Flight</button>
+            <div className="flight-form__container itinerary-form__container">
+                <form className="flight-form itinerary-form" onSubmit={handleSubmit}>
+                    <div className="modal-title">Add Flight</div>
+                    <div className="flexy">
+                        <label className="form-label">
+                            Date of Travel
+                        </label>
+                        <input
+                            type="date"
+                            value={date}
+                            placeholder="Trip Date"
+                            className="itinerary-form-input"
+                            onChange={(e) => setDate(e.target.value)}
+                        />
+                    </div>
+                    <div className="flexy">
+                        <label className="form-label">
+                            Origin (Airport)
+                        </label>
+                        <input
+                            type="string"
+                            value={origin}
+                            placeholder="Origin"
+                            className="itinerary-form-input"
+                            onChange={(e) => setOrigin(e.target.value)}
+                        />
+                    </div>
+                    <div className="flexy">
+                        <label className="form-label">
+                            Destination (Airport)
+                        </label>
+                        <input
+                            type="string"
+                            value={destination}
+                            placeholder="Destination"
+                            className="itinerary-form-input"
+                            onChange={(e) => setDestination(e.target.value)}
+                        />
+                    </div>
+                    <div className="flexy">
+                        <label className="form-label">
+                            Departure Time
+                        </label>
+                        <input
+                            type="string"
+                            value={departure}
+                            placeholder="Departure"
+                            className="itinerary-form-input"
+                            onChange={(e) => setDeparture(e.target.value)}
+                        />
+                    </div>
+                    <div className="flexy">
+                        <label className="form-label">
+                            Arrival Time
+                        </label>
+                        <input
+                            type="string"
+                            value={arrival}
+                            placeholder="Arrival"
+                            className="itinerary-form-input"
+                            onChange={(e) => setArrival(e.target.value)}
+                        />
+                    </div>
+                    <div className="flexy">
+                        <label className="form-label">
+                            Airline
+                        </label>
+                        <input
+                            type="string"
+                            value={airline}
+                            placeholder="Airline"
+                            className="itinerary-form-input"
+                            onChange={(e) => setAirline(e.target.value)}
+                        />
+                    </div>
+                    <div className="flexy">
+                        <label className="form-label">
+                            Flight Number
+                        </label>
+                        <input
+                            type="string"
+                            value={flight_no}
+                            placeholder="Flight No."
+                            className="itinerary-form-input"
+                            onChange={(e) => setFlight_no(e.target.value)}
+                        />
+                    </div>
+                    <div className="flexy">
+                        <label className="form-label">
+                            Notes
+                        </label>
+                        <input
+                            type="text"
+                            value={notes}
+                            placeholder="Notes"
+                            className="itinerary-form-input"
+                            onChange={(e) => setNotes(e.target.value)}
+                        />
+                    </div>
+                    <div className="form-button-div">
+                        <button type="submit" className="form-button">New Flight</button>
+                    </div>
                 </form>
             </div>
         </>
